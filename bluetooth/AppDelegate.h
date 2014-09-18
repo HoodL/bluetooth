83a672997fe54794a7707615ab5090c0297c6fb3
Lihui <baotong@baotongtekiMacBook-Pro.local>
2014-09-18 19:29:19 +0800

    Initial Commit

---
+++ bluetooth/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  bluetooth
+//
+//  Created by Lihui on 14-9-18.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
